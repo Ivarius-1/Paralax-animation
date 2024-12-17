f4355146ae797c1b114b55e98d521c8804040da1
Ivarius-1 <ivan8012000@gmail.com>
2024-12-17 19:35:38 +1000

    Add files via upload

---
+++ Лекция 16_Parallax_GSAP_study/script.js
@@ -0,0 +1,43 @@
+gsap.to('#bg',{
+    scrollTrigger : {
+        scrub:1
+    },
+    scale: 1.5
+})
+
+gsap.to('#man',{
+    scrollTrigger : {
+        scrub:1
+    },
+    scale: 0.5
+})
+
+gsap.to('#mountain_left',{
+    scrollTrigger: {
+        scrub:1
+    },
+    x:-500
+})
+gsap.to('#mountain_right',{
+    scrollTrigger: {
+        scrub:1
+    },
+    x:500
+})
+
+gsap.to('#text',{
+    scrollTrigger:{
+        scrub: 1
+    },
+    y:500
+})
+gsap.to('#text2',{
+    scrollTrigger:{
+        trigger: "#text2",
+        start: "top 80%",
+        end: "top 50%",
+        scrub: 1,
+        markers: true
+    },
+    x:500
+})
